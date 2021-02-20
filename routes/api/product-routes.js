4bd7f8ec3b8af11f23f2365805918e1cabeb566a
Cory Calaway <cory.c.calaway@gmail.com>
2021-02-20 15:57:45 -0700

    first working get request on products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,12 +6,26 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
+  Product.findAll({
+    include: [
+      {
+        model: Category,
+        attributes: ['category_name']
+      },
+      {
+        model:Tag,
+        attributes: ['tag_name']
+      }
+    ]
+  })
+  .then(answer => res.json(answer))
   // be sure to include its associated Category and Tag data
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
+  
   // be sure to include its associated Category and Tag data
 });
 
