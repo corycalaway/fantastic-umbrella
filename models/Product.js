0fd78e2f4132ff7a783b3eae1b62d89b43bc2779
Cory Calaway <cory.c.calaway@gmail.com>
2021-02-20 15:38:43 -0700

    initial models setup

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,39 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+   id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    allowNull: false,
+    autoIncrement: true
+
+   },
+   product_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+   },
+   price: {
+    type: DataTypes.DECIMAL,
+    allowNull: false,
+    validate: {
+      isDecimal: true
+    }
+   },
+   stock: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 10,
+    validate: {
+      isNumeric: true
+    }
+   },
+   category_id: {
+     type: DataTypes.INTEGER,
+     references: {
+       model: 'category',
+       key: 'id'
+     }
+   }
   },
   {
     sequelize,
@@ -21,3 +53,8 @@ Product.init(
 );
 
 module.exports = Product;
+
+// product_name: 'Plain T-Shirt',
+//     price: 14.99,
+//     stock: 14,
+//     category_id: 1,
