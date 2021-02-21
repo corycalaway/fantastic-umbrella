6795a20a59139898ac5dc93e8ff1d62b342f6509
Cory Calaway <cory.c.calaway@gmail.com>
2021-02-21 14:27:48 -0700

    finished routes for category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -55,14 +55,61 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  console.log(req.body)
+  Category.create({
+    
+    category_name: req.body.category_name
+  })
+
+  .then(categoryInfo =>
+    res.json(categoryInfo))
+
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+
 });
+
+
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  console.log(req.body)
+  Category.update(
+    {
+      category_name: req.body.category_name
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    })
+    .then(categoryInfo => {
+      res.json(categoryInfo);
+    })
+
+
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
 
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+  
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(categoryInfo => {
+      res.json(categoryInfo);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
 
 module.exports = router;
